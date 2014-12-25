2dddb4ce6461dbf406d1b0e034da863dbd00fb00
Vova <vovakats@yahoo.com>
2014-12-25 01:06:28 -0500

    gitignore

---
+++ Huge.Prerender.Scheduler/JobManager.cs
@@ -0,0 +1,22 @@
+﻿using Huge.Prerender.Models.Scheduler;
+using Huge.Prerender.Scheduler.Jobs;
+using Quartz;
+
+namespace Huge.Prerender.Scheduler
+{
+    public class JobManager
+    {
+        public void CreateJob(SchedulerSettingElements config, JobData jobData)
+        {
+            IJobDetail job = JobBuilder.Create<PrerenderJob>()
+                .WithIdentity(config.Key)
+                .StoreDurably(true).Build();
+            job.JobDataMap.Put("info", jobData);
+            ITrigger trigger = TriggerBuilder.Create().WithCronSchedule(config.CronExpression).Build();
+
+            var scheduler = QuartzScheduler.Instance;
+            scheduler.ScheduleJob(job, trigger);
+            scheduler.Start();
+        }
+    }
+}
